21c31b7331c80ad9c7e69726b6f9588d13148f41
AliDalla95 <shnieder.d95@gmail.com>
2020-06-01 21:13:52 +0300

    First

---
+++ sender/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace sender
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_phone = new System.Windows.Forms.TextBox();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_message = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt_phone
+            // 
+            this.txt_phone.Location = new System.Drawing.Point(178, 34);
+            this.txt_phone.Name = "txt_phone";
+            this.txt_phone.Size = new System.Drawing.Size(176, 20);
+            this.txt_phone.TabIndex = 0;
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(98, 209);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(256, 31);
+            this.btn_send.TabIndex = 3;
+            this.btn_send.Text = "Send";
+            this.btn_send.UseVisualStyleBackColor = true;
+            this.btn_send.Click += new System.EventHandler(this.btn_send_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(95, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Phone Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(95, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Message:";
+            // 
+            // txt_message
+            // 
+            this.txt_message.Location = new System.Drawing.Point(178, 86);
+            this.txt_message.Multiline = true;
+            this.txt_message.Name = "txt_message";
+            this.txt_message.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txt_message.Size = new System.Drawing.Size(176, 95);
+            this.txt_message.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 277);
+            this.Controls.Add(this.txt_message);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.txt_phone);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_phone;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_message;
+    }
+}
+
---
+++ sender/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace sender
+{
+    public partial class Form1 : Form
+    {
+        SerialPort sp = new SerialPort();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_send_Click(object sender, EventArgs e)
+        {
+            sp.PortName = "COM8";
+            sp.ReadTimeout = 2000;
+            sp.Open();
+            sp.Write("AT\r");
+            sp.Write("AT+CMGF=1\r");
+            System.Threading.Thread.Sleep(1500);
+            sp.Write(txt_message.Text + "\x1A");
+            MessageBox.Show("Done","Message",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            sp.Close();
+        }
+    }
+}
